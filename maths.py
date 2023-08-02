e1b78eb8a54e8927bb16046daf06a9b136b3dfac
Samyam Aryal <samyam.aryal01@gmail.com>
2023-08-02 13:32:06 +0545

    Added function for subtraction

--- maths.py
+++ maths.py
@@ -1,3 +1,7 @@
 def add(a, b):
     '''Take two numbers as argument, add them and return the sum'''
     return a+b
+
+def subtract(a, b):
+    '''Take two numbers as argument, subtract the second number from the first and return the sum'''
+    return a-b
