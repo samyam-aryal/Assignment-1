8c564255c01c90b9503f4d77c545d494507e8512
samyam-aryal <samyam.aryal@fusemachines.com>
2023-08-02 21:36:10 +0545

    feat: Added functions for addition, subtraction and multiplication.

---
+++ maths.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    '''Take two numbers as argument, add them and return the sum'''
+    return a+b
+
+def subtract(a, b):
+    '''Take two numbers as argument, subtract the second number from the first and return the sum'''
+    return a-b
+
+
+def multiply(a, b):
+    '''Take two numbers as argument and return their product'''
+    return a*b
