90cd1c7b7b8fb48213ac7661c8d4cfbb061fe520
samyam-aryal <samyam.aryal@fusemachines.com>
2023-08-02 22:19:02 +0545

    docs: Improved docstrings of functions

--- maths.py
+++ maths.py
@@ -1,17 +1,51 @@
 def add(a, b):
-    '''Take two numbers as argument, add them and return the sum'''
+    '''Add two numbers
+
+    Parameters:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The sum of a and b.
+    '''
     return a+b
 
 def subtract(a, b):
-    '''Take two numbers as argument, subtract the second number from the first and return the sum'''
+    '''Subtract the second number from the first
+    
+    Parameters:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The result of subtracting b from a.
+    '''
     return a-b
 
 
 def multiply(a, b):
-    '''Take two numbers as argument and return their product'''
+    '''
+    Multiply two numbers.
+
+    Parameters:
+        a (int or float): The first number.
+        b (int or float): The second number.
+
+    Returns:
+        int or float: The product of a and b.
+    '''
     return a*b
 
 def divide(a, b):
-    '''Takes two arguments a & b, and returns a/b'''
+    '''
+    Divide the first number by the second.
+
+    Parameters:
+        a (int or float): The numerator.
+        b (int or float): The denominator.
+
+    Returns:
+        float: The result of dividing a by b.
+    '''
     return a/b
 
